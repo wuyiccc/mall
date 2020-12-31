cfdf82306886d322ca6ffbc2c28450a4b06017db
wuyiccc <3406324191@qq.com>
2020-12-31 19:10:26 +0800

    封装sessionStorage

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import storage from '@/storage';
 
-    export default {
-        name: 'App',
-        components: {},
-        data() {
-            return {}
-        },
-        mounted() {
-        }
-    }
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {}
+  },
+  mounted() {
+    // storage.setItem('a', 1);
+    storage.setItem('user', {a: 1});
+    // storage.setItem('abc', {a: 1}, 'user');
+    storage.clear('a', 'user');
+  },
+}
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/storage/index.js
+++ src/storage/index.js
@@ -3,3 +3,59 @@
  * @date  2020/10/17 16:02
  * 岂曰无衣，与子同袍~
  */
+const STORAGE_KEY = 'mall';
+
+export default {
+    // 存储值
+    setItem(key, value, module_name) {
+
+        if (module_name) {
+            let val = this.getItem(module_name);
+            val[key] = value;
+            this.setItem(module_name, val);
+        } else {
+            let val = this.getStorage();
+            val[key] = value;
+            this.setStorage(val);
+        }
+
+    },
+
+    getItem(key, module_name) {
+        if (module_name) {
+            let val = this.getItem(module_name);
+            if (val) return val[key];
+        }
+
+        return this.getStorage()[key];
+    },
+    /**
+     * 根据key获取sessionStorage，这里key默认为STORAGE_KEY
+     * @returns value: JSON对象
+     */
+    getStorage() {
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}');
+    },
+    /**
+     * 根据默认的key设置值
+     * @param value: JSON对象
+     */
+    setStorage(value) {
+        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+    },
+    /**
+     * 删除key对应的value, 或者 module_name:key对应的value
+     * @param key
+     * @param module_name
+     */
+
+    clear(key, module_name) {
+        let val = this.getStorage();
+        if (module_name) {
+            delete val[module_name][key];
+        } else {
+            delete val[key];
+        }
+        this.setStorage(val);
+    }
+};
